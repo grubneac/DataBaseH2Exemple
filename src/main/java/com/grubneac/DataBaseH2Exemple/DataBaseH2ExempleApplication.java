d6908d1e96c0da1924410916525e7688bb643fc1
Grubneac <grubneac@yandex.ru>
2020-12-25 17:53:34 +0200

    init files

---
+++ src/main/java/com/grubneac/DataBaseH2Exemple/DataBaseH2ExempleApplication.java
@@ -0,0 +1,13 @@
+package com.grubneac.DataBaseH2Exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseH2ExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseH2ExempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grubneac/DataBaseH2Exemple/DataBaseH2ExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grubneac.DataBaseH2Exemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseH2ExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
