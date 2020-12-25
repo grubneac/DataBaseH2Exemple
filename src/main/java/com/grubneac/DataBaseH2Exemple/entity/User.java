d2f8d5e612ce7dec3971b41b6b70b056e1469c5f
Grubneac <grubneac@yandex.ru>
2020-12-25 17:46:47 +0200

    entity file

---
+++ src/main/java/com/grubneac/DataBaseH2Exemple/entity/User.java
@@ -0,0 +1,67 @@
+package com.grubneac.DataBaseH2Exemple.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity(name = "user")
+public class User {
+    @Id
+    @Column
+    private long userId;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean enabled;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId && enabled == user.enabled && username.equals(user.username) && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, password, enabled);
+    }
+}
